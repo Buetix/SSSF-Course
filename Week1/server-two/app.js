95fbdc85e685c66db3a5fdf565df6372b6886812
Lassi Ylirautalahti <lassiy@metropolia.fi>
2020-03-21 18:22:27 +0200

    #4 Express stuff

---
+++ Week1/server-two/app.js
@@ -0,0 +1,16 @@
+﻿const express = require('express');
+const app = express();
+const port = 3000;
+const pug = require('pug');
+app.use(express.static('public'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+    res.render('index', {
+        message: 'Click on the cat', 
+        catName: 'Frank',
+        age: 'Age: 6', 
+        weight: 'Weight: 5'})
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
