f364ab7e12f6d325062bdb398b34077f01a83613
Lassi Ylirautalahti <lassiy@metropolia.fi>
2020-03-22 13:24:23 +0200

    #4 Added some of Week2 assignments on Routing

---
+++ Week2/app.js
@@ -0,0 +1,22 @@
+'use strict';
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/cat', (req, res) => {
+  res.send('From this endpoint you can get cats.')
+});
+
+app.get('/POST', (req, res) => {
+  res.send('With this endpoint you can add cats.')
+});
+
+app.get('/PUT', (req, res) => {
+  res.send('With this endpoint you can edit cats.')
+});
+
+app.get('/DELETE', (req, res) => {
+  res.send('With this endpoint you can delete cats.')
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
