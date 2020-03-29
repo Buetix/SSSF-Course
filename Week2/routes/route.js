2307feed0b1ee49f06b8b46119e4cf75c06c6e54
Lassi Ylirautalahti <lassiy@metropolia.fi>
2020-03-29 18:53:06 +0300

    #6 Can now get cat by name

--- Week2/routes/route.js
+++ Week2/routes/route.js
@@ -6,7 +6,7 @@ router.route('/cat').post( async (req, res) => {
     console.log('now we here');
     const myCat = await cat.create( {
         name: 'TestCat',
-        age: 2,
+        age: 5,
         weight: 3,
         gender: 'male',
         color: 'grey'
@@ -14,8 +14,11 @@ router.route('/cat').post( async (req, res) => {
     res.send(`cat created with id: ${myCat._id}`);
 });
 
-router.route('/').get( async (req, res) => {
-    res.send(await cat.find());
+router.route('/cat/:name').get( async (req, res) => {
+    cat.find({name: req.params.name}, function (err, data){
+       if(err) console.log(err);
+       res.json(data); 
+    });
 });
 
 module.exports = router;
